e6d26b733e520409ad8bbb8898b64ba94e72aabe
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-07-14 12:39:06 +0300

    up

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const kingBooksEl = document.getElementById("king-books");
+const kingBooksEl = document.getElementById("king-books")
+const loaderDOM = document.querySelector('.loader');
+const tableDOM = document.getElementById('table')
+
+tableDOM.classList.add('hide');
+
+loaderDOM.classList.add('show');
+
+
+
 
 fetch("https://stephen-king-api.onrender.com/api/books")
-    .then((atsakymas) => atsakymas.json())
+  .then((atsakymas) => atsakymas.json())
   .then((duomenys) => {
     console.log(duomenys);
+
+    tableDOM.classList.remove('hide');
+
+    loaderDOM.classList.remove('show');
 
     duomenys.data.forEach((book) => {
       kingBooksEl.insertAdjacentHTML(
         "beforeend",
-        `<tr>
+        `<tr data-bookid-"${book.id}">
                 <td>${book.Title}</td>
                 <td>${book.Year}</td>
                 <td>${book.Publisher}</td>
@@ -17,11 +30,14 @@ fetch("https://stephen-king-api.onrender.com/api/books")
                 <td>${book.Notes[0]}</td>
                 <td>${book.created_at}</td>
                 <td>${book.villains.length}</td>
-        
-        
+                <td>${book.Notes[0] ? book.Notes.join('; '):'No notes'}</td>
             </tr>`
       );
     });
   })
   .catch((error) => console.log(error));
- 
+kingBooksEl.addEventListener('click', e => {
+  console.log(e.target.parentElement.dataset.bookid);
+  const bid=e.target.parentElement.dataset.bookid
+fetch("https://stephen-king-api.onrender.com/api/books"+bid).then
+})
